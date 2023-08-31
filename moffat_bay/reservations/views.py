37530dc585cdeef153222c9d6cc05c6456337372
AcidEdge <markwitt83@yahoo.com>
2023-08-31 17:53:39 -0400

    added text swap message for mailist
    added the text swap message for mailing list signup. message now replaces the signup message once submitted.

--- moffat_bay/reservations/views.py
+++ moffat_bay/reservations/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from account.forms import MailListForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -7,6 +8,7 @@ def home(request):
     if request.method == "POST":
         if mailform.is_valid():
             mailform.save()
+            messages.success(request, "Thank you for signing up for our mailing list!")
             return redirect('reservations-home')
     context = {
         'title':'Landing Page',
